c36e4badb2133b4e2de0b155e3b2520005f703c2
brunograssano <grassanobruno@gmail.com>
2020-12-19 14:12:20 -0300

    Expando Naive Bayes

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import OrdinalEncoder
 
 
 def prepararSetDeDatos(info_fiumark_df: pd.DataFrame, usuario_volveria_df: pd.DataFrame):
@@ -17,6 +18,9 @@ def prepararSetDeDatos(info_fiumark_df: pd.DataFrame, usuario_volveria_df: pd.Da
     info_fiumark_df = pd.concat([sufijos_y_nombres, info_fiumark_df], axis='columns')
 
     # Trabajo con valores nulos
+    sede_mas_frecuente = info_fiumark_df['nombre_sede'].value_counts().index[0]
+    info_fiumark_df['nombre_sede'].fillna(sede_mas_frecuente, inplace=True)
+
     info_fiumark_df['fila'].fillna("No responde", inplace=True)
 
     mediana_edad_senioras = info_fiumark_df[info_fiumark_df['sufijo'] == 'Señora'].edad.dropna().median()
@@ -45,5 +49,21 @@ def prepararSetDeDatos(info_fiumark_df: pd.DataFrame, usuario_volveria_df: pd.Da
     info_fiumark_df["genero"] = info_fiumark_df["genero"].astype("category")
     info_fiumark_df["nombre_sede"] = info_fiumark_df["nombre_sede"].astype("category")
     info_fiumark_df["fila"] = info_fiumark_df["fila"].astype("category")
+    info_fiumark_df["sufijo"] = info_fiumark_df["sufijo"].astype("category")
+
+    info_fiumark_df.drop(columns=['id_usuario'], inplace=True)
 
     return info_fiumark_df, usuario_volveria_df
+
+
+def categoricalNBPreprocessing(fiumark_procesado_df: pd.DataFrame):
+    """Preparara y dejara listo para usar un dataframe en el modelo de NB categorico.
+     Necesita que venga ya preprocesado anteriormente por la funcion del TP1"""
+
+    fiumark_procesado_df.drop(columns=['nombre', 'edad', 'precio_ticket', 'id_ticket', 'autocompletamos_edad'], inplace=True)
+    # Sacamos estas columnas, ya que no tiene sentido calcular la probabilidad de cada una de ellas. Por ejemplo, cual es la probabilidad de que te llames de tal forma.
+
+    encoder = OrdinalEncoder()
+    encoder.fit(fiumark_procesado_df)
+
+    return encoder.transform(fiumark_procesado_df)
