a2cf600827bf6ed5e42a9dc4dcb6015f742e661f
brunograssano <grassanobruno@gmail.com>
2021-02-10 17:17:09 -0300

    Agrego preprocesado nuevo en KNN

--- TP2/preprocessing.py
+++ TP2/preprocessing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.cluster import KMeans
 
 ################ CONFIGURACIONES INICIALES ################
 
@@ -84,26 +85,76 @@ def codificacionOneHot(datos_a_codificar):
 def normalizar(datos_juntos):
     return (datos_juntos - datos_juntos.mean()) / datos_juntos.std()
 
+def estratificar_edades(edad):
+    if(edad <= 10):
+        return 'ninio'
+    elif(edad <=25):
+        return 'joven'
+    elif(edad <= 50):
+        return 'adulto'
+    return 'mayor'
+
+def estratificar_precios(total_pagado):
+    if total_pagado <= 1:
+        return 'Pago poco'
+    elif total_pagado <= 6:
+        return 'Pago normal'
+    elif total_pagado <= 12:
+        return 'Pago mucho'
+    return 'Pago demasiado'
+
+
+def categorizar_invitados(cantidad_invitados):
+    if cantidad_invitados == 0:
+        return 'Fue solo'
+    return 'Fue en grupo'
+
 ################ PREPROCESSING ################
 
-def conversionAVariablesNumericasNormalizadas(fiumark_procesado_df):
+def expansionDelDataset(info_fiumark_df: pd.DataFrame):
+    """
+
+    """
+    nombres, fiumark_numerico = conversionAVariablesNumericas(info_fiumark_df)
+    info_fiumark_df['2_clusters'] = KMeans(n_clusters=2, random_state=0).fit_predict(fiumark_numerico)
+    info_fiumark_df['4_clusters'] = KMeans(n_clusters=4, random_state=0).fit_predict(fiumark_numerico)
+    info_fiumark_df['10_clusters'] = KMeans(n_clusters=10, random_state=0).fit_predict(fiumark_numerico)
+
+    info_fiumark_df['cantidad_total_invitados'] = info_fiumark_df['parientes'] + info_fiumark_df['amigos']
+
+    info_fiumark_df['total_pagado'] = (info_fiumark_df['cantidad_total_invitados'] + 1 ) * info_fiumark_df['precio_ticket']
+
+    info_fiumark_df['pago_categorizado'] = info_fiumark_df['total_pagado'].apply(estratificar_precios)
+
+    info_fiumark_df['edades_estratificadas'] = info_fiumark_df['edad'].apply(estratificar_edades)
+
+    info_fiumark_df['categoria_invitados'] = info_fiumark_df['cantidad_total_invitados'].apply(categorizar_invitados)
+
+    return info_fiumark_df
+
+
+
+def conversionAVariablesNumericasNormalizadas(fiumark_procesado_df,columnas_codificables_extra = [],columnas_numericas_extra = []):
     """ Necesita que el dataset fiumark ya venga preprocesado por la funcion prepararSetDeDatos (aplica las transformaciones del TP1)
         Convertira todas las variables a valores numericos para que puedan ser usados en los modelos. Una vez hecho eso, los normalizara."""
 
-    nombres_de_los_features, datos_juntos = conversionAVariablesNumericas(fiumark_procesado_df)
+    nombres_de_los_features, datos_juntos = conversionAVariablesNumericas(fiumark_procesado_df,columnas_codificables_extra,columnas_numericas_extra)
     return normalizar(datos_juntos)
 
-def conversionAVariablesNumericas(fiumark_procesado_df):
+def conversionAVariablesNumericas(fiumark_procesado_df,columnas_codificables_extra = [],columnas_numericas_extra = []):
     """ Necesita que el dataset fiumark ya venga preprocesado por la funcion prepararSetDeDatos (aplica las transformaciones del TP1)
         Si se cumple la precondicion, se encargara de dejar de forma numerica todas las features.
         Los valores categoricos seran convertidos mediante OneHotEncoding.
         Al momento de devolverlos, lo hace junto a los nombres de los features, por si se quiere recuperar luego el DataFrame."""
 
-    datos_a_codificar = fiumark_procesado_df[['sufijo', 'tipo_de_sala', 'genero', 'autocompletamos_edad', 'fila', 'nombre_sede']]
+    columnas_a_codificar = ['sufijo', 'tipo_de_sala', 'genero', 'autocompletamos_edad', 'fila', 'nombre_sede'] + columnas_codificables_extra
+    datos_a_codificar = fiumark_procesado_df[columnas_a_codificar]
     datos_codificados, nombres_de_los_features_codificados = codificacionOneHot(datos_a_codificar)
-    datos_numericos = fiumark_procesado_df[['edad', 'amigos', 'parientes', 'precio_ticket']]
+
+    columnas_numericas = ['edad', 'amigos', 'parientes', 'precio_ticket'] + columnas_numericas_extra
+    datos_numericos = fiumark_procesado_df[columnas_numericas]
     datos_juntos = np.hstack((np.array(datos_numericos), datos_codificados))
-    nombres_de_los_features = ['edad', 'amigos', 'parientes', 'precio_ticket'] + nombres_de_los_features_codificados.tolist()
+    nombres_de_los_features = columnas_numericas + nombres_de_los_features_codificados.tolist()
     return nombres_de_los_features, datos_juntos
 
 def conversionAVariablesCategoricasCodificadas(fiumark_procesado_df: pd.DataFrame):
