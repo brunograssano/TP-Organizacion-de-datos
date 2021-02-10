b167dc34db747aecf7be443b5921fb4b7f16bb7b
brunograssano <grassanobruno@gmail.com>
2021-02-10 15:50:51 -0300

    Se usan los datasets del drive

--- TP2/funcionesAuxiliares.py
+++ TP2/funcionesAuxiliares.py
@@ -6,6 +6,18 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_curve
 from sklearn.metrics import confusion_matrix
 
+def obtenerDatasetDeUrl(url):
+    direccion = 'https://drive.google.com/uc?export=download&id=' + url.split('/')[-2]
+    return pd.read_csv(direccion)
+
+def obtenerDatasets():
+    y = obtenerDatasetDeUrl('https://drive.google.com/file/d/1km-AEIMnWVGqMtK-W28n59hqS5Kufhd0/view?usp=sharing')
+    X = obtenerDatasetDeUrl('https://drive.google.com/file/d/1i-KJ2lSvM7OQH0Yd59bX01VoZcq8Sglq/view?usp=sharing')
+    return X, y
+
+def obtenerHoldout():
+    return obtenerDatasetDeUrl('https://drive.google.com/file/d/1I980-_K9iOucJO26SG5_M8RELOQ5VB6A/view?usp=sharing')
+
 def mostrarROCCurve(modelo,nombreModelo,X_test, X_train, y_test, y_train):
     fpr_test, tpr_test, thresholds_test = roc_curve(y_test, modelo.predict_proba(X_test)[:, 1])
     fpr_train, tpr_train, thresholds_train = roc_curve(y_train, modelo.predict_proba(X_train)[:, 1])
